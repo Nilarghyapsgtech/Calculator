1cf730e561e75288be2cf43490b57fe82ab74119
Nilarghya Sarkar <21z372csepsgtech@gmail.com>
2023-06-23 17:45:04 +0530

    Github

---
+++ calculator.js
@@ -0,0 +1,84 @@
+const keys = document.querySelectorAll('.key');
+const display_input = document.querySelector('.display .input');
+const display_output = document.querySelector('.display .output');
+
+ let input="";
+ for (let i = 0; i <keys.length; i++) {
+    const key=keys[i];
+    const value=key.dataset.key;
+    key.addEventListener('click',()=>{
+        if(value=="clear"){
+            input="";
+            display_input.innerHTML="";
+            display_output.innerHTML="";
+        }
+        else if(value=="backspace"){
+            input=input.slice(0,-1);
+            display_input.innerHTML=CleanInput(input);
+        }
+        else if(value=="="){
+            const result=eval(prepareInput(input));
+            display_output.innerHTML=result;
+           
+        }
+        else if(value=="brackets"){
+            if(input.indexOf("(")==-1 || input.indexOf("(")!=-1 && input.indexOf(")")!=-1 && input.indexOf("(")>input.indexOf(")")  ){
+                input=input+"(";
+            }
+            else if(input.indexOf("(")!=-1 && input.indexOf(")")==-1 ||input.indexOf("(")!=-1 && input.indexOf(")")!=-1 && input.indexOf("(")>input.indexOf(")")){
+                input=input+")";
+            }
+            display_input.innerHTML=CleanInput(input);
+        }
+       else {
+        if(validInput(value)){
+        input=input+value;
+        display_input.innerHTML=CleanInput(input);
+
+        }
+       }
+    })
+ }
+ function CleanInput(input){
+    let input_array=input.split("");
+    let input_array_length = input_array.length;
+    for (let i = 0; i < input_array_length; i++) {
+		if (input_array[i] == "*") {
+			input_array[i] = ` <span class="operator">x</span> `;
+		} else if (input_array[i] == "/") {
+			input_array[i] = ` <span class="operator">÷</span> `;
+		} else if (input_array[i] == "+") {
+			input_array[i] = ` <span class="operator">+</span> `;
+		} else if (input_array[i] == "-") {
+			input_array[i] = ` <span class="operator">-</span> `;
+		} else if (input_array[i] == "(") {
+			input_array[i] = `<span class="brackets">(</span>`;
+		} else if (input_array[i] == ")") {
+			input_array[i] = `<span class="brackets">)</span>`;
+		} else if (input_array[i] == "%") {
+			input_array[i] = `<span class="percent">%</span>`;
+		}
+	}
+    return input_array.join("");
+ }
+function validInput(value){
+   
+    let last_input=input.slice(-1);
+    let operators=["+","-","*","/","."];
+    if(operators.includes(value) && operators.includes(last_input)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function prepareInput(input){
+    let prepare_input=input.split("");
+    for (let index = 0; index <prepare_input.length; index++) {
+       if(prepare_input[index]=="%"){
+        prepare_input[index]="/100";
+       }
+        
+    }
+    return prepare_input.join("");
+}
